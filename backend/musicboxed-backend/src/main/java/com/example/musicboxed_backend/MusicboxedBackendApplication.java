5cba4f59750ff93c92b143f498fc46cf3c0a2c8c
reiy-git <reiybriones.edu@gmail.com>
2026-05-29 15:36:24 +0800

    feat: added liquibase and also added springboot and fix the docker-database connection

---
+++ backend/musicboxed-backend/src/main/java/com/example/musicboxed_backend/MusicboxedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicboxed_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicboxedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicboxedBackendApplication.class, args);
+	}
+
+}
---
+++ backend/musicboxed-backend/src/test/java/com/example/musicboxed_backend/MusicboxedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicboxed_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicboxedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
